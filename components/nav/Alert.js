37181675e0ecb58bef81c7682fbeb67b7df30f86
LUIS ANAYA <49276647+laanayam333@users.noreply.github.com>
2021-02-13 21:54:28 +0100

    took off div of alert component

--- components/nav/Alert.js
+++ components/nav/Alert.js
@@ -1,23 +1,21 @@
 const Alert = ({ preview }) => {
   return (
-    <div>
-      <div className="py-2 text-center text-sm">
-        {preview ? (
-          <>
-            This page is a preview.{' '}
-            <a
-              href="/api/exit-preview"
-              className="underline hover:text-cyan duration-200 transition-colors"
-            >
-              Click here
-            </a>{' '}
-            to exit preview mode.
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
+    <>
+      {preview ? (
+        <>
+          This page is a preview.{' '}
+          <a
+            href="/api/exit-preview"
+            className="underline hover:text-cyan duration-200 transition-colors"
+          >
+            Click here
+          </a>{' '}
+          to exit preview mode.
+        </>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
